a6b4f421d7b0c4936e1ea89221af97f6c47ddb11
ayushrakesh <ayushrakesh35711artlover@gmail.com>
2023-11-10 11:30:38 +0530

    Understanding functions

--- main.go
+++ main.go
@@ -3,6 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of cards"
-	fmt.Println(card)
+	// var card string = "Ace of cards"
+	// cards := "Ace of Spades"
+
+	cards := []string{"Ace of Spades", "King of Heart", newCard()}
+	cards = append(cards, "Six of Heart")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+}
+
+func newCard() string {
+	return "Four of Diamonds"
 }
